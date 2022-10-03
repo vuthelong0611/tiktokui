e183f8985e5a1da99db47868db81845b4cb02e6a
vuthelong0611 <vuthelong0611@gmail.com>
2022-10-03 15:25:28 +0700

    a

--- src/components/Layout/components/Video/index.js
+++ src/components/Layout/components/Video/index.js
@@ -1,14 +1,50 @@
+import { Button } from "bootstrap";
 import classNames from "classnames/bind";
-import { useRef } from "react";
+import { useRef,useEffect,useState } from "react";
 import { Link } from "react-router-dom";
+import useElementOnScreen from "../../../../hooks/video";
 import styles from "./Video.module.scss";
 
 const cx = classNames.bind(styles);
 function Video({ data }) {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+  const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.3
+  }
+  const isVisibile = useElementOnScreen(options, videoRef)
+  const onVideoClick = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(!playing);
+    } else {
+      videoRef.current.play();
+      setPlaying(!playing);
+    }
+  };
+  useEffect(() => {
+    if (isVisibile) {
+      if (!playing) {        
+        videoRef.current.play();
+        setPlaying(true)
+      }
+    }
+    else {
+      if (playing) {        
+        videoRef.current.pause();
+        setPlaying(false)
+      }
+    }
+  }, [isVisibile])
+
+  
   return (
     <div className={cx("avatar")}>
-      <video controls className={cx("video")}>
+      <video controls ref= {videoRef} className={cx("video")} loop preload="true" onClick={onVideoClick} >
         <source src={data.file_url} type="video/mp4" />
+       
       </video>
     </div>
   );
